fbc15929607a1bd075bf5b5e0ac1ab8a4995548a
Ingy döt Net <ingy@ingy.net>
2022-10-29 15:47:33 -0700

    Version 0.1.34

--- lib/bashplus.bash
+++ lib/bashplus.bash
@@ -4,7 +4,7 @@
 # * Many are improved versions of existing builtins/commands.
 
 
-bashplus:version() ( echo '0.1.33' )
+bashplus:version() ( echo '0.1.34' )
 
 
 # Define these first for use within:
