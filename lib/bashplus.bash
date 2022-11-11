db58b595f13737d4fbf76b2ff04c7880042d87a7
Ingy döt Net <ingy@ingy.net>
2022-11-11 06:18:17 -0800

    Version 0.1.49

--- lib/bashplus.bash
+++ lib/bashplus.bash
@@ -4,7 +4,7 @@
 # * Many are improved versions of existing builtins/commands.
 
 
-bashplus:version() ( echo '0.1.48' )
+bashplus:version() ( echo '0.1.49' )
 
 bashplus:main() {
   bashplus_lib=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
